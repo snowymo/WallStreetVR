4ea41d96aa4cf2bec83e2155ab0a60364edb881f
Zhenyi He <snowymohehe@gmail.com>
2017-05-07 01:15:11 -0400

    change particle color real-timely and rc support

---
+++ Assets/Scripts/BrushCtrl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrushCtrl : MonoBehaviour {
+
+	public Transform arcam;
+
+	public Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = arcam.position + offset;
+	}
+}
--- Assets/Scripts/SprayerCtrl.cs
+++ Assets/Scripts/SprayerCtrl.cs
@@ -3,17 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //[ExecuteInEditMode]
+using System;
+
+
 public class SprayerCtrl : MonoBehaviour {
 
     public ParticleSystem part;
     public List<ParticleCollisionEvent> collisionEvents;
     public GameObject particle;
+	public ParticleSystemRenderer partRender;
 
 	// these lists are used to contain the particles which match
 	// the trigger conditions each frame.
 	List<ParticleSystem.Particle> enter = new List<ParticleSystem.Particle>();
 	List<ParticleSystem.Particle> exit = new List<ParticleSystem.Particle>();
 	bool isPaint = false;
+
+	Color paintColor;
+	TextMesh tm;
 
 	public void Paint(){
 		isPaint = true;
@@ -24,13 +31,43 @@ public class SprayerCtrl : MonoBehaviour {
         part = GetComponent<ParticleSystem>();
         part.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
         collisionEvents = new List<ParticleCollisionEvent>();
+		paintColor = Color.yellow;
+		tm = GetComponent<TextMesh> ();
+
+		//partRender = part.GetComponent<ParticleSystemRenderer>();
 
     }
 
     void Update()
     {
-		if (Input.GetKeyDown (KeyCode.Space)) {
+		var main = part.main;
+
+		if (Input.GetKeyDown (KeyCode.Space) || Input.GetKeyDown (KeyCode.Joystick1Button7)) {
 			isPaint = true;
+		} else if (Input.GetKeyDown (KeyCode.Joystick1Button2) || Input.GetKeyDown(KeyCode.R)) {//x
+			paintColor = Color.red;
+			//partRender.material.color = paintColor;
+			//print (partRender.material.color);
+
+			main.startColor = paintColor;
+			//part.Emit(emitParams, 60);
+
+		}
+		else if (Input.GetKeyDown (KeyCode.Joystick1Button3)|| Input.GetKeyDown(KeyCode.Y)) { //y
+			paintColor = Color.yellow;
+			main.startColor = paintColor;
+		}
+		else if (Input.GetKeyDown (KeyCode.Joystick1Button5)|| Input.GetKeyDown(KeyCode.B)) { //b
+			paintColor = Color.blue;
+			main.startColor = paintColor;
+		}
+		else if (Input.GetKeyDown (KeyCode.Joystick1Button10)|| Input.GetKeyDown(KeyCode.G)) {//a
+			paintColor = Color.green;
+			main.startColor = paintColor;
+		}
+		else if (Input.GetKeyDown (KeyCode.Joystick1Button11)|| Input.GetKeyDown(KeyCode.M)) {//=
+			paintColor = Color.magenta;
+			main.startColor = paintColor;
 		}
 		if(isPaint)
         {
@@ -39,7 +76,19 @@ public class SprayerCtrl : MonoBehaviour {
             part.Play();
             StartCoroutine(wait());
         }
+		detectPressedKeyOrButton ();
     }
+
+	public void detectPressedKeyOrButton()
+	{
+		foreach(KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
+		{
+			if (Input.GetKeyDown (kcode)) {
+				Debug.Log ("KeyCode down: " + kcode);
+				//tm.text = kcode.ToString();
+			}
+		}
+	}
 
     IEnumerator wait()
     {
@@ -62,12 +111,12 @@ public class SprayerCtrl : MonoBehaviour {
 					Vector3 force = collisionEvents [i].velocity * 10;
 					//rb.AddForce(force);
 					print (i + " has rigidbody:" + other.name);
-					other.GetComponent<Renderer> ().material.color = Color.yellow;
+					other.GetComponent<Renderer> ().material.color = paintColor;
 				} else {
 					print (i + " no rigidbody:" + other.name);
 					//GameObject go = GameObject.Instantiate(particle);
 					//go.transform.position = collisionEvents[i].intersection;
-					other.GetComponent<Renderer> ().material.color = Color.red;
+					other.GetComponent<Renderer> ().material.color = paintColor;
 				}
 			} else {
 				print ("other collider:" + other.name);
@@ -90,7 +139,7 @@ public class SprayerCtrl : MonoBehaviour {
 			print(i + " trigger:" + enter[i]);
 			//other.GetComponent<Renderer> ().material.color = Color.yellow;
 			p.velocity = new Vector3();
-			p.startColor = Color.yellow;
+			p.startColor = paintColor;
 			enter[i] = p;
 		}
 
