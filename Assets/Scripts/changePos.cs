700304c3ccf993fe18057d1ae8c24419529e424b
Zhenyi He <snowymohehe@gmail.com>
2017-05-08 18:11:01 -0400

    change model space to close to (0,0,0)

---
+++ Assets/Scripts/changePos.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class changePos : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var a = new List<string>(); 
+		foreach (Transform child in transform){
+			a.Add (child.name);
+//			foreach(Transform grandchild in child)
+//			{
+//				if (grandchild.name.Contains ("MHN")) {
+//					print (grandchild.name);
+//					child.position += new Vector3 (-55, 0, -90);
+//					break;
+//				}
+//			}	
+		}
+		print (string.Join (" ", a.ToArray ()));
+		//transform.position += new Vector3 (-55, 0, -90);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
